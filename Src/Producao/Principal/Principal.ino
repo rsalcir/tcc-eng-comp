6fc7f3842f2fd327855e5404b7eaf7cbaf793aac
Alcir Rodrigues dos Santos Junior <rsalcir@gmail.com>
2015-10-25 00:28:16 -0300

    re-adicionando codigo de procucao

---
+++ Src/Producao/Principal/Principal.ino
@@ -0,0 +1,106 @@
+#include "SIM900.h"
+#include "call.h"
+#include "sms.h"
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+
+CallGSM call;
+SMSGSM sms;
+boolean conectadoARedeGSM = false;
+boolean atendeuAChamada = false;
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+
+#define botaoDaCampainha 13
+#define fechadura 12
+
+char pos;
+char message[180];
+char number[13];
+
+void escreveNoDisplay(String primeiraLinha, String segundaLinha){
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print(primeiraLinha);
+   lcd.setCursor(0,1);
+   lcd.print(segundaLinha);
+}
+
+void configuraEntradasESaidas(){
+  pinMode(botaoDaCampainha,INPUT);
+  pinMode(fechadura, OUTPUT);
+}
+
+void iniciaDisplay(){
+  lcd.begin (16,2);
+  lcd.setBacklight(HIGH);
+}
+
+boolean verificaConexaoComARedeGSM(){
+ return gsm.begin(2400);
+}
+ 
+void setup()
+{
+  configuraEntradasESaidas();
+  iniciaDisplay();
+  escreveNoDisplay("Iniciando","Aguarde...");
+  conectadoARedeGSM = verificaConexaoComARedeGSM();
+}
+
+boolean botaoFoiPressionado(){
+ return digitalRead(botaoDaCampainha)==HIGH;
+}
+
+void realizaUmaChamada(char *numeroTelefonico){
+ call.Call(numeroTelefonico);
+}
+
+void encerraUmaChamada(){
+ call.HangUp();
+}
+
+void loop()
+{
+  byte position;
+char *phone_number;
+
+  gsm.GetPhoneNumber(position,phone_number);
+  char *numeroTelefonico = "909092874764";
+  if(conectadoARedeGSM){
+    escreveNoDisplay("Sistema","Ativado");
+    delay(1000);
+    
+     if(atendeuAChamada == false && botaoFoiPressionado()){
+       escreveNoDisplay("Ligando para",numeroTelefonico);
+       realizaUmaChamada(numeroTelefonico);
+       atendeuAChamada = true;
+     }else if(atendeuAChamada && botaoFoiPressionado()) {
+       escreveNoDisplay("Encerrando","chamada");
+       encerraUmaChamada();
+       atendeuAChamada = false;
+     }
+
+ 
+        pos=sms.IsSMSPresent(SMS_UNREAD);
+
+       if((int)pos>0 && (int)pos<=20) {
+        
+         message[0]='\0';
+         sms.GetSMS((int)pos,number,message,180);
+         sms.DeleteSMS(pos);
+        
+          if(strstr(message,"1")){
+             escreveNoDisplay("Abrindo","fechadura");
+             for (int i=0; i<10; i++){
+               digitalWrite(fechadura,HIGH);
+               delay(100);
+               digitalWrite(fechadura,LOW);
+               delay(100);
+             }
+          }
+       }
+  }
+}
+
+
